9daf51946354d87c2aed5d150e9e4ca13a8c582b
Young Joong Kim <yjkimjunior@berkeley.edu>
2015-10-29 16:09:23 -0700

    mobile

--- src/application/views.py
+++ src/application/views.py
@@ -24,83 +24,82 @@ from models import ExampleModel
 # Flask-Cache (configured to use App Engine Memcache API)
 cache = Cache(app)
 
+@app.route("/")
+def index():
+    return render_template('base.html')
 
-def home():
-    return redirect(url_for('list_examples'))
-
-
-def say_hello(username):
-    """Contrived example to demonstrate Flask's url routing capabilities"""
-    return 'Hello %s' % username
-
-
-@login_required
-def list_examples():
-    """List all examples"""
-    examples = ExampleModel.query()
-    form = ExampleForm()
-    if form.validate_on_submit():
-        example = ExampleModel(
-            example_name=form.example_name.data,
-            example_description=form.example_description.data,
-            added_by=users.get_current_user()
-        )
-        try:
-            example.put()
-            example_id = example.key.id()
-            flash(u'Example %s successfully saved.' % example_id, 'success')
-            return redirect(url_for('list_examples'))
-        except CapabilityDisabledError:
-            flash(u'App Engine Datastore is currently in read-only mode.', 'info')
-            return redirect(url_for('list_examples'))
-    return render_template('list_examples.html', examples=examples, form=form)
+# def say_hello(username):
+#     """Contrived example to demonstrate Flask's url routing capabilities"""
+#     return 'Hello %s' % username
 
 
-@login_required
-def edit_example(example_id):
-    example = ExampleModel.get_by_id(example_id)
-    form = ExampleForm(obj=example)
-    if request.method == "POST":
-        if form.validate_on_submit():
-            example.example_name = form.data.get('example_name')
-            example.example_description = form.data.get('example_description')
-            example.put()
-            flash(u'Example %s successfully saved.' % example_id, 'success')
-            return redirect(url_for('list_examples'))
-    return render_template('edit_example.html', example=example, form=form)
+# @login_required
+# def list_examples():
+#     """List all examples"""
+#     examples = ExampleModel.query()
+#     form = ExampleForm()
+#     if form.validate_on_submit():
+#         example = ExampleModel(
+#             example_name=form.example_name.data,
+#             example_description=form.example_description.data,
+#             added_by=users.get_current_user()
+#         )
+#         try:
+#             example.put()
+#             example_id = example.key.id()
+#             flash(u'Example %s successfully saved.' % example_id, 'success')
+#             return redirect(url_for('list_examples'))
+#         except CapabilityDisabledError:
+#             flash(u'App Engine Datastore is currently in read-only mode.', 'info')
+#             return redirect(url_for('list_examples'))
+#     return render_template('list_examples.html', examples=examples, form=form)
 
 
-@login_required
-def delete_example(example_id):
-    """Delete an example object"""
-    example = ExampleModel.get_by_id(example_id)
-    if request.method == "POST":
-        try:
-            example.key.delete()
-            flash(u'Example %s successfully deleted.' % example_id, 'success')
-            return redirect(url_for('list_examples'))
-        except CapabilityDisabledError:
-            flash(u'App Engine Datastore is currently in read-only mode.', 'info')
-            return redirect(url_for('list_examples'))
+# @login_required
+# def edit_example(example_id):
+#     example = ExampleModel.get_by_id(example_id)
+#     form = ExampleForm(obj=example)
+#     if request.method == "POST":
+#         if form.validate_on_submit():
+#             example.example_name = form.data.get('example_name')
+#             example.example_description = form.data.get('example_description')
+#             example.put()
+#             flash(u'Example %s successfully saved.' % example_id, 'success')
+#             return redirect(url_for('list_examples'))
+#     return render_template('edit_example.html', example=example, form=form)
 
 
-@admin_required
-def admin_only():
-    """This view requires an admin account"""
-    return 'Super-seekrit admin page.'
+# @login_required
+# def delete_example(example_id):
+#     """Delete an example object"""
+#     example = ExampleModel.get_by_id(example_id)
+#     if request.method == "POST":
+#         try:
+#             example.key.delete()
+#             flash(u'Example %s successfully deleted.' % example_id, 'success')
+#             return redirect(url_for('list_examples'))
+#         except CapabilityDisabledError:
+#             flash(u'App Engine Datastore is currently in read-only mode.', 'info')
+#             return redirect(url_for('list_examples'))
 
 
-@cache.cached(timeout=60)
-def cached_examples():
-    """This view should be cached for 60 sec"""
-    examples = ExampleModel.query()
-    return render_template('list_examples_cached.html', examples=examples)
+# @admin_required
+# def admin_only():
+#     """This view requires an admin account"""
+#     return 'Super-seekrit admin page.'
 
 
-def warmup():
-    """App Engine warmup handler
-    See http://code.google.com/appengine/docs/python/config/appconfig.html#Warming_Requests
+# @cache.cached(timeout=60)
+# def cached_examples():
+#     """This view should be cached for 60 sec"""
+#     examples = ExampleModel.query()
+#     return render_template('list_examples_cached.html', examples=examples)
 
-    """
-    return ''
+
+# def warmup():
+#     """App Engine warmup handler
+#     See http://code.google.com/appengine/docs/python/config/appconfig.html#Warming_Requests
+
+#     """
+#     return ''
 
